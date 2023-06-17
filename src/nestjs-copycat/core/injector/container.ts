5f70d0b6f910697a6296c7cd65f8f6ca7fe4623a
odoldotol <odoldotol@gmail.com>
2023-06-18 04:16:55 +0900

    인스턴스 만들기

--- src/nestjs-copycat/core/injector/container.ts
+++ src/nestjs-copycat/core/injector/container.ts
@@ -1,1 +1,29 @@
-export class NestContainer {}
+import { Type } from "src/nestjs-copycat/common/interfaces";
+
+export class NestContainer {
+
+  private readonly instances = new Map<string, any>();
+
+  public addInstance(key: string, instance: any) {
+    if (this.instances.has(instance.name))
+      throw new Error(`Duplicated provider name: ${instance.name}`);
+    this.instances.set(key, instance);
+  }
+
+  public getInstance(name: string) {
+    const instance = this.instances.get(name);
+    if (!instance)
+      throw new Error(`Provider not found: ${name}`);
+    return instance;
+  }
+
+  public getDependencies(cls: Type<any>) {
+    return Reflect.getMetadata('design:paramtypes', cls)
+    .map((dep: Type<any>) => this.getInstance(dep.name)) || [];
+  }
+
+  get instancesMap() { // 임시
+    return this.instances;
+  }
+
+}
--- src/nestjs-copycat/core/nest-factory.ts
+++ src/nestjs-copycat/core/nest-factory.ts
@@ -1,18 +1,54 @@
 const express = require('express')
 import { Express } from "express";
 import { NestContainer } from './injector';
+import { Type } from "../common/interfaces";
 
 export class NestFactoryStatic {
 
   public async create(
-    moduleCls: any,
+    moduleCls: Type<any>,
   ) {
+    
+    const container = new NestContainer();
+
+    this.moduleInit(moduleCls, container);
+
+    console.log(container.instancesMap);
+
+
+
     const app = express();
     app.use(express.json());
     return app;
 
-    const container = new NestContainer();
   }
+
+  private moduleInit(module: Type<any>, container: NestContainer) {
+    
+    Reflect.getMetadata('imports', module)
+    .forEach((importedModule: Type<any>) => {
+      this.moduleInit(importedModule, container);
+    });
+
+    Reflect.getMetadata('providers', module)
+    .forEach((provider: Type<any>) => {
+      // Todo: 인젝터블 확인
+      // Todo: addInstance 하기전에 getDependencies 해서 가져온 의존성들이 본인의 프로바이더 또는 하위 모듈에서 내보내진 프로바이더인지 확인해야함.
+      const instance = new provider(...container.getDependencies(provider));
+      container.addInstance(provider.name, instance);
+    });
+
+    Reflect.getMetadata('controllers', module)
+    .forEach((controller: Type<any>) => {
+      // Todo: 컨트롤러 확인
+      const instance = new controller(...container.getDependencies(controller));
+      container.addInstance(controller.name, instance);
+      // Todo: 라우터 만들기
+    });
+  
+  }
+
+
 
 }
 
