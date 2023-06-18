b11b2decc579b858817bb34d24b8e6a974b6404b
odoldotol <odoldotol@gmail.com>
2023-06-18 14:06:16 +0900

    라우터 만들어줄 매서드 찾기

--- src/nestjs-copycat/core/nest-factory.ts
+++ src/nestjs-copycat/core/nest-factory.ts
@@ -10,24 +10,24 @@ export class NestFactoryStatic {
   ) {
     
     const container = new NestContainer();
-
-    this.moduleInit(moduleCls, container);
-
-    console.log(container.instancesMap);
-
-
-
     const app = express();
+
+    await this.moduleInit(moduleCls, container, app);
+
+    // console.log(container.instancesMap);
+
+
+
     app.use(express.json());
     return app;
 
   }
 
-  private moduleInit(module: Type<any>, container: NestContainer) {
+  private async moduleInit(module: Type<any>, container: NestContainer, app: Express) {
     
     Reflect.getMetadata('imports', module)
     .forEach((importedModule: Type<any>) => {
-      this.moduleInit(importedModule, container);
+      this.moduleInit(importedModule, container, app);
     });
 
     Reflect.getMetadata('providers', module)
@@ -35,6 +35,7 @@ export class NestFactoryStatic {
       // Todo: 인젝터블 확인
       // Todo: addInstance 하기전에 getDependencies 해서 가져온 의존성들이 본인의 프로바이더 또는 하위 모듈에서 내보내진 프로바이더인지 확인해야함.
       const instance = new provider(...container.getDependencies(provider));
+      // Todo: 이때 onModuleInit 메서드 가졌으면 실행해주기?
       container.addInstance(provider.name, instance);
     });
 
@@ -43,7 +44,15 @@ export class NestFactoryStatic {
       // Todo: 컨트롤러 확인
       const instance = new controller(...container.getDependencies(controller));
       container.addInstance(controller.name, instance);
-      // Todo: 라우터 만들기
+      
+      // 라우터 만들어서 app에 등록
+      const router = express.Router();
+      const path = Reflect.getMetadata('path', controller);
+      console.log(path);
+      const prototype = Object.getPrototypeOf(instance);
+      const propertyNames = Object.getOwnPropertyNames(prototype);
+      propertyNames.splice(propertyNames.indexOf('constructor'), 1);
+      console.log(propertyNames);
     });
   
   }
