c9e6420f86ee32aefe7d8d1f2ecda375c7c2b319
odoldotol <odoldotol@gmail.com>
2023-06-23 21:07:47 +0900

    Refact: moduleInit 함수를 Container 로 옮기기

--- src/nestjs-copycat/core/nest-factory.ts
+++ src/nestjs-copycat/core/nest-factory.ts
@@ -2,7 +2,7 @@ const express = require('express');
 import { Express } from "express";
 import { NestContainer } from './injector';
 import { Type } from "../common/interfaces";
-import { MODULE_METADATA, PATH_METADATA, METHOD_METADATA } from "../common/constants";
+import { PATH_METADATA, METHOD_METADATA } from "../common/constants";
 import { RequestMethod } from '../common';
 import { pathParser } from "../common";
 
@@ -26,46 +26,8 @@ export class NestFactoryStatic {
   }
 
   private async initialize(module: any, container: NestContainer) {
-    await this.moduleInit(module, container);
+    await container.moduleInit(module, container);
   };
-
-  private readonly moduleMap = new Map<string, any>(); // 컨테이너로 옮기기 // 임시
-  // Todo: imports, providers, controllers 가 undefined 인 경우라도 문제없이 초기화 해야함.
-  // Todo: 내부함수로 나누기, 일부 내부함수를 컨테이너로 옮기기
-  private async moduleInit(module: Type<any>, container: NestContainer) {
-
-    this.moduleMap.set(module.name, module); // 임시
-
-    await Promise.all(Reflect.getMetadata(MODULE_METADATA.IMPORTS, module).map(
-      async (importedModule: Type<any>) => {
-        // Todo: 이미 한번 쳐다본 모듈은 다시 보지 말아야함 // <- 컨테이너가 모듈을 다루도록 하는것이 선행되어야함.
-        this.moduleMap.has(importedModule.name) || // 임시
-        await this.moduleInit(importedModule, container);
-      }
-    ));
-
-    await Promise.all(Reflect.getMetadata(MODULE_METADATA.PROVIDERS, module).map(
-      async (provider: Type<any>) => {
-        // Todo: 인젝터블 확인
-        // Todo: getDependencies 해서 가져온 의존성들이 본인의 프로바이더 또는 imported module 의 exported provider 인지 확인해야함.
-        const instance = new provider(...container.getDependencies(provider));
-        // Todo: 이때 onModuleInit 메서드 가졌으면 실행해주기?
-        container.addInstance(provider.name, instance);
-      }
-    ));
-
-    await Promise.all(Reflect.getMetadata(MODULE_METADATA.CONTROLLERS, module).map(
-      async (controller: Type<any>) => {
-        // Todo: 컨트롤러 확인
-        // Todo: getDependencies 해서 가져온 의존성들이 본인의 프로바이더 또는 하위 모듈에서 내보내진 프로바이더인지 확인해야함.
-        const instance = new controller(...container.getDependencies(controller));
-        container.addInstance(controller.name, instance);
-      }
-    ));
-
-    console.log(module.name, "Init");
-  
-  }
 
   private resolveRoutes(container: NestContainer, app: Express) {
     container.getControllers().forEach((instance) => {
